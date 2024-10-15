98c8c89ac8f356f9f011b524a8c312001de58b6a
Spdrcrv6tt <69010177+Spdrcrv6tt@users.noreply.github.com>
2024-10-15 17:35:21 -0500

    Update ascii_clock.py

---
+++ ascii_clock.py
@@ -0,0 +1,30 @@
+
+#num1[1] would indiacate first row of the number 1
+#number 1 would look like
+# num1[1]
+# num1[2]
+# num1[3]
+# num1[4]
+# num1[5]
+#
+#Taking input as STRING, will reevaluate at time of recombination
+#
+# NUMBERVARIABLE is the variable for the time intake
+
+
+first = NUMBERVARIABLE[1]
+second = NUMBERVARIABLE[2]
+third = NUMBERVARIABLE[3]
+fourth = NUMBERVARIABLE[4]
+
+a = 
+b = 
+c = 
+d = 
+
+
+number_1 = a
+for number in number_1:
+    print(number)
+
+    
